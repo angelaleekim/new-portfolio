794a155be45f340b9cbd4766123e99611ec5a91d
angelakim <akim173@calpoly.edu>
2025-09-07 16:11:03 -0700

    Import font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
 import '@mantine/core/styles.css';
 
 import React from 'react';
-import { Outfit } from 'next/font/google';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import { theme } from '../theme';
-
-const outfit = Outfit({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-});
 
 export const metadata = {
   title: 'Angela Kim',
@@ -20,13 +14,17 @@ export default function RootLayout({ children }: { children: any }) {
     <html lang="en">
       <head>
         <ColorSchemeScript />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
         <link rel="shortcut icon" href="/favicon.svg" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
       </head>
-      <body className={outfit.className}>
+      <body>
         <MantineProvider theme={theme}>{children}</MantineProvider>
       </body>
     </html>
